c84c39b57fb31688a268176cddf5fe25f9ec6186
Rajuuunaidu <46774383+Rajuuunaidu@users.noreply.github.com>
2019-04-27 19:53:28 +0530

    Update Roads_And_Libraries.java

--- Roads_And_Libraries.java
+++ Roads_And_Libraries.java
@@ -3,50 +3,20 @@ package week6;
 import java.util.*;
 public class Roads_And_Libraries {
 	
-
-    public static long solve(int n, long cl, long cr, ArrayList<TreeSet<Integer>> adj) {
-        if (cl <= cr)
-         {
-              return (cl * n);
-         }
-        long minimum_cost = 0;
-        boolean[] visited_nodes = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (!visited_nodes[i]) {
-                long temp_Cost = cl;
-                Stack<Integer> stack = new Stack<>();
-                stack.push(i);
-                visited_nodes[i] = true;
-                while (!stack.empty()) {
-                    int u = stack.pop();
-                    Iterator<Integer> it = adj.get(u).iterator();
-                    while (it.hasNext()) {
-                        int v = it.next();
-                        if (!visited_nodes[v]) {
-                            stack.push(v);
-                            visited_nodes[v] = true;
-                            temp_Cost += cr;
-                        }
-                    }
-                }
-                minimum_cost += temp_Cost;
-            }
-        }
-        
-        return minimum_cost;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int q = scanner.nextInt();
-        for(int a0 = 0; a0 < q; a0++){
+        int quries = scanner.nextInt();
+        for(int queries_count = 0; queries_count < quries; queries_count++){
             int n =scanner.nextInt();
             int m = scanner.nextInt();
-            long cl = scanner.nextLong();
-            long cr = scanner.nextLong();
+            long city_library = scanner.nextLong();
+            long city_road = scanner.nextLong();
             ArrayList<TreeSet<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < n; i++) { adj.add(new TreeSet<Integer>()); }
-            for(int i = 0; i < m; i++){
+            for (int i = 0; i < n; i++) { 
+            	adj.add(new TreeSet<Integer>()); 
+            	}
+            for(int i = 0; i < m; i++)
+            {
                 int start = scanner.nextInt();
                 start--;
                 int end = scanner.nextInt();
@@ -54,7 +24,39 @@ public class Roads_And_Libraries {
                 adj.get(start).add(end);
                 adj.get(end).add(start);
             }
-            System.out.println(Roads_And_Libraries.solve(n, cl, cr, adj));
+            if (city_library <= city_road)
+            {
+                 System.out.println(city_library*n);
+                 
+            }
+            else
+            {
+           long minimum_cost = 0;
+           boolean[] visited_nodes = new boolean[n];
+           for (int i = 0; i < n; i++) {
+               if (!visited_nodes[i]) {
+                   long temp_Cost = city_library;
+                   Stack<Integer> stack = new Stack<>();
+                   stack.push(i);
+                   visited_nodes[i] = true;
+                   while (!stack.empty()) {
+                       int u = stack.pop();
+                       Iterator<Integer> it = adj.get(u).iterator();
+                       while (it.hasNext()) {
+                           int v = it.next();
+                           if (!visited_nodes[v]) {
+                               stack.push(v);
+                               visited_nodes[v] = true;
+                               temp_Cost += city_road;
+                           }
+                       }
+                   }
+                   minimum_cost += temp_Cost;
+               }
+           }
+           
+           System.out.println(minimum_cost);
+            }
         }
         scanner.close();
     }
